1a9d09054bd853d957e1649c83be4ab7bf3cb3bd
Eduardo Pooch <e.pais.pooch@nki.nl>
2023-04-12 15:41:35 +0200

    show queued jobs -q

--- nodestat.py
+++ nodestat.py
@@ -5,10 +5,13 @@ import math
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-j", "--jobs", help="show active jobs on the nodes", action="store_true")
-parser.add_argument("-m", "--me", help="show my active jobs", action="store_true")
+parser.add_argument("-m", "--me", help="show only my jobs", action="store_true")
+parser.add_argument("-q", "--queue", help="show jobs in the queue", action="store_true")
 
 show_jobs = parser.parse_args().jobs
 show_my_jobs = parser.parse_args().me
+show_queue = parser.parse_args().queue
+
 assert not (show_jobs and show_my_jobs), "You can't use -j and --me at the same time"
 
 def parse_tres(tres_str):
@@ -108,88 +111,121 @@ if show_jobs or show_my_jobs:
 
 print("{:<15}{:<15}{:<12}{:<10}{:<8}{:<10}".format("PARTITION", "NODE", "CPUS", "GPUS", "MEM (G)", " | JOBS" if show_jobs or show_my_jobs else " "))
 
+partitions = set([info['partition'] for node_name, info in node_info.items()])
+partitions = sorted(partitions)
 
-for node_name, info in sorted(node_info.items(), key=lambda x: x[1]['partition']):
-    available_cpu = int(info['cfg_tres']['cpu']) - int(info['alloc_tres']['cpu'])
-    total_cpu = int(info['cfg_tres']['cpu'])
-    total_cpu = "\033[90m" + "/" + str(total_cpu) + "\033[0m"
-    if available_cpu == 0:
-        available_cpu = "\033[91m" + str(available_cpu) + "\033[0m"
-    else:
-        available_cpu = "\033[32m" + str(available_cpu) + "\033[0m"
-    available_cpu = f"{available_cpu}{total_cpu}"
+for partition in partitions:
+    info_partition = [x for x in node_info.items() if x[1]['partition'] == partition]
+    for node_name, info in info_partition:
+        available_cpu = int(info['cfg_tres']['cpu']) - int(info['alloc_tres']['cpu'])
+        total_cpu = int(info['cfg_tres']['cpu'])
+        total_cpu = "\033[90m" + "/" + str(total_cpu) + "\033[0m"
+        if available_cpu == 0:
+            available_cpu = "\033[91m" + str(available_cpu) + "\033[0m"
+        else:
+            available_cpu = "\033[32m" + str(available_cpu) + "\033[0m"
+        available_cpu = f"{available_cpu}{total_cpu}"
 
-    available_gpu = int(info['cfg_tres']['gres/gpu']) - int(info['alloc_tres']['gres/gpu'])
-    total_gpu = int(info['cfg_tres']['gres/gpu'])
-    total_gpu = "\033[90m" + "/" + str(total_gpu) + "\033[0m"
-    if available_gpu == 0:
-        available_gpu = "\033[91m" + str(available_gpu) + "\033[0m"
-    else:
-        available_gpu = "\033[32m" + str(available_gpu) + "\033[0m"
-    available_gpu = f"{available_gpu}{total_gpu}"
+        available_gpu = int(info['cfg_tres']['gres/gpu']) - int(info['alloc_tres']['gres/gpu'])
+        total_gpu = int(info['cfg_tres']['gres/gpu'])
+        total_gpu = "\033[90m" + "/" + str(total_gpu) + "\033[0m"
+        if available_gpu == 0:
+            available_gpu = "\033[91m" + str(available_gpu) + "\033[0m"
+        else:
+            available_gpu = "\033[32m" + str(available_gpu) + "\033[0m"
+        available_gpu = f"{available_gpu}{total_gpu}"
 
-    available_mem = parse_mem(info['cfg_tres']['mem']) - parse_mem(info['alloc_tres']['mem'])
-    total_mem = parse_mem(info['cfg_tres']['mem'])
-    total_mem = "\033[90m" + "/" + str(total_mem) + "\033[0m"
-    if available_mem == 0:
-        available_mem = "\033[91m" + str(available_mem) + "\033[0m"
-    else:
-        available_mem = "\033[32m" + str(available_mem) + "\033[0m"
+        available_mem = parse_mem(info['cfg_tres']['mem']) - parse_mem(info['alloc_tres']['mem'])
+        total_mem = parse_mem(info['cfg_tres']['mem'])
+        total_mem = "\033[90m" + "/" + str(total_mem) + "\033[0m"
+        if available_mem == 0:
+            available_mem = "\033[91m" + str(available_mem) + "\033[0m"
+        else:
+            available_mem = "\033[32m" + str(available_mem) + "\033[0m"
 
-    available_mem = f"{available_mem}{total_mem}"
-    state = info['state']
+        available_mem = f"{available_mem}{total_mem}"
+        state = info['state']
 
-    if state != 'IDLE' and state != 'MIXED' and state != 'ALLOCATED':
-        available_cpu = "\033[90m"  + "SUSPENDED" + "\033[0m" + "\033[32m" + "" + "\033[0m"
-        available_gpu = "\033[91m"  + " " + "\033[0m" + "\033[32m" + "" + "\033[0m"
-        available_mem = "\033[91m"  + " " + "\033[0m" + "\033[32m" + "" + "\033[0m"
+        if state != 'IDLE' and state != 'MIXED' and state != 'ALLOCATED':
+            available_cpu = "\033[90m"  + "SUSPENDED" + "\033[0m" + "\033[32m" + "" + "\033[0m"
+            available_gpu = "\033[91m"  + " " + "\033[0m" + "\033[32m" + "" + "\033[0m"
+            available_mem = "\033[91m"  + " " + "\033[0m" + "\033[32m" + "" + "\033[0m"
 
+        out = "{:<15}{:<15}{:<30}{:<28}{:<26}{}".format(info['partition'], node_name, available_cpu, available_gpu, available_mem, " | " if show_jobs or show_my_jobs else " ")
+        if show_jobs or show_my_jobs:
+            if show_jobs:
+                result = subprocess.run(["squeue", "-o", " %.12u %C %b %m %i", "--nodelist=" + node_name], stdout=subprocess.PIPE, universal_newlines=True)
+            if show_my_jobs:
+                result = subprocess.run(["squeue", "-o", " %.12j %C %b %m %i", "--me" ,"--nodelist=" + node_name], stdout=subprocess.PIPE, universal_newlines=True)
+            
+            text = result.stdout
+            text = text.split('\n')
+
+            if len(text) > 1:
+                for line in text[1:-1]:
+                    line = line.strip()
+                    if line != "":
+                        user, cpu, gpu, mem, jobid = line.split()
+                        jobid = jobid.split('_')[0]
+                        gpu = job_info[jobid]['tres']['gres/gpu']
+                        mem = job_info[jobid]['tres']['mem']
+                        mem = parse_mem(mem)
+
+                        total_gpu = info['cfg_tres']['gres/gpu']
+                        recommended_cpu = default_values[node_name]['DefCpuPerGPU'] * int(gpu) if int(gpu) > 0 else 2                
+                        recommended_mem = parse_mem(str(default_values[node_name]['DefMemPerCPU'] * int(cpu)) + "M")
+                        
+                        if int(cpu) <= recommended_cpu:
+                            cpu = "\033[33m" + cpu + "\033[0m"
+                        else: 
+                            cpu = "\033[91m" + cpu + "\033[0m"
+                        
+                        if mem <= recommended_mem:
+                            mem = "\033[33m" + str(mem) + "G" + "\033[0m"
+                        else:
+                            mem = "\033[91m" + str(mem) + "G" + "\033[0m"
+
+                        if gpu == "0": #gray
+                            gpu = "\033[90m" + gpu + "\033[0m"
+                        else:
+                            gpu = "\033[33m" + gpu + "\033[0m"
+
+                        #bold user 
+                        user = "\033[1m" + user + "\033[0m"
+                    
+                        res = f"{cpu}:{gpu}:{mem}"
+
+                        out += f"{user}({res}), "
+                out = out[:-2] if out.endswith(", ") else out
+        print(out)
     
-    out = "{:<15}{:<15}{:<30}{:<28}{:<26}{}".format(info['partition'], node_name, available_cpu, available_gpu, available_mem, " | " if show_jobs or show_my_jobs else " ")
-    if show_jobs or show_my_jobs:
-        if show_jobs:
-            result = subprocess.run(["squeue", "-o", " %.12u %C %b %m %i", "--nodelist=" + node_name], stdout=subprocess.PIPE, universal_newlines=True)
+    #print queued jobs
+    if show_queue:
+        result = subprocess.run(["squeue", "-o", " %.12u %i %R", "--partition=" + partition], stdout=subprocess.PIPE, universal_newlines=True)
         if show_my_jobs:
-            result = subprocess.run(["squeue", "-o", " %.12j %C %b %m %i", "--me" ,"--nodelist=" + node_name], stdout=subprocess.PIPE, universal_newlines=True)
+            result = subprocess.run(["squeue", "-o", " %.12j %i %R", "--me" ,"--partition=" + partition], stdout=subprocess.PIPE, universal_newlines=True)
         
         text = result.stdout
         text = text.split('\n')
-
+        queued_jobs = []
         if len(text) > 1:
             for line in text[1:-1]:
                 line = line.strip()
                 if line != "":
-                    user, cpu, gpu, mem, jobid = line.split()
-                    jobid = jobid.split('_')[0]
-                    gpu = job_info[jobid]['tres']['gres/gpu']
-                    mem = job_info[jobid]['tres']['mem']
-                    mem = parse_mem(mem)
+                    user, jobid, reason = line.split()
+                    if reason.startswith("("):
+                        queued_jobs.append(line)
+        if len(queued_jobs) > 0:
+            out = " ᶫ"
+        for line in queued_jobs:
+            user, jobid, reason = line.split()
+            #yellow reason
+            reason = "\033[33m" + reason + "\033[0m"
 
-                    total_gpu = info['cfg_tres']['gres/gpu']
-                    recommended_cpu = default_values[node_name]['DefCpuPerGPU'] * int(gpu) if int(gpu) > 0 else 2                
-                    recommended_mem = parse_mem(str(default_values[node_name]['DefMemPerCPU'] * int(cpu)) + "M")
-                    
-                    if int(cpu) <= recommended_cpu:
-                        cpu = "\033[33m" + cpu + "\033[0m"
-                    else: 
-                        cpu = "\033[91m" + cpu + "\033[0m"
-                    
-                    if mem <= recommended_mem:
-                        mem = "\033[33m" + str(mem) + "G" + "\033[0m"
-                    else:
-                        mem = "\033[91m" + str(mem) + "G" + "\033[0m"
-
-                    if gpu == "0": #gray
-                        gpu = "\033[90m" + gpu + "\033[0m"
-                    else:
-                        gpu = "\033[33m" + gpu + "\033[0m"
-
-                    #bold user 
-                    user = "\033[1m" + user + "\033[0m"
-                   
-                    res = f"{cpu}:{gpu}:{mem}"
-
-                    out += f"{user}({res}), "
+            res = f"{user}{reason}"
+            #italic
+            res = "\033[3m" + res + "\033[0m"
+            out += f"{res}, "
             out = out[:-2] if out.endswith(", ") else out
-
-    print(out)
+            print(out)
+                        
